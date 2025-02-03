4bcd76ddd0d2ba0a7ee50ae5af369bad3ccf631e
endersuray19 <endersurya19@gmail.com>
2025-02-03 19:14:41 +0800

    app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+app.get('/users/:id', (req, res) => {
+    console.log(`Query Parameters : ${JSON.stringify(req.query)}`);
+    console.log(`Headers : ${JSON.stringify(req.query)}`);
+    console.log(`Method: ${req.method}`);
+    res.status(202).header({ 'X-Custom-Header': 'foo', 'X-Custom-Header-2': 'fuu' }).send(`Hello ID ${req.params.id}`);
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,22 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/',(req, res)=>{
+    res.send('Hello World');
+});
+
+app.get('/health',(req, res)=>{
+    res.send('OK');
+})
+
+app.get('/users/:id',(req, res)=>{
+    console.log(`Query Parameters : ${JSON.stringify(req.query)}`);
+    console.log(`Headers : ${JSON.stringify(req.query)}`);
+    console.log(`Method: ${req.method}`);
+    res.status(202).header({'X-Custom-Header':'foo', 'X-Custom-Header-2':'fuu'}).send(`Hello ID ${req.params.id}`);
+})
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
+
