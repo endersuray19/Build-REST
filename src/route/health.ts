436923708f1ad21cdccaae7d161eb993ee30b260
endersuray19 <endersurya19@gmail.com>
2025-02-04 21:43:21 +0800

    Error Handling

--- src/route/health.ts
+++ src/route/health.ts
@@ -3,6 +3,7 @@ import { Request, Router } from "express";
 export const router = Router();
 
 router.get('/health',(req:Request, res)=>{
+    throw new Error('Aplication error');
     res.send({message:'OK'});
 
 })
